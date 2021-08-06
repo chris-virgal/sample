12732040fe2e374f157b6b5fcdc04b68885fe888
chris <chris.virgal@gmail.com>
2021-08-07 00:18:15 +0200

    test sampl

--- src/app.js
+++ src/app.js
@@ -1,38 +1,11 @@
 /* global algoliasearch instantsearch */
-import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js';
+
 
 const searchClient = algoliasearch('VTWN9LBTBB', '374b76e5ea3101f81afb7aa69ff9515e');
 
 const search = instantsearch({
   indexName: 'test Chris',
   searchClient,
-});
-
-autocomplete({
-  container: '.autocomplete',
-  placeholder: 'Search for products',
-  getSources({ query }) {
-    return [
-      {
-        sourceId: 'products',
-        getItems() {
-          return getAlgoliaResults({
-            searchClient,
-            queries: [
-              {
-                indexName: 'instant_search',
-                query,
-                params: {
-                  hitsPerPage: 3,
-                },
-              },
-            ],
-          });
-        },
-
-      },
-    ];
-  },
 });
 
 search.addWidgets([
