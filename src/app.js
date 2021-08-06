140456bc9962ab6ddf4157c356d41ee16f47c494
chris <chris.virgal@gmail.com>
2021-08-07 00:14:14 +0200

    test sampl

--- src/app.js
+++ src/app.js
@@ -1,10 +1,38 @@
 /* global algoliasearch instantsearch */
+import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js';
 
 const searchClient = algoliasearch('VTWN9LBTBB', '374b76e5ea3101f81afb7aa69ff9515e');
 
 const search = instantsearch({
   indexName: 'test Chris',
   searchClient,
+});
+
+autocomplete({
+  container: '.autocomplete',
+  placeholder: 'Search for products',
+  getSources({ query }) {
+    return [
+      {
+        sourceId: 'products',
+        getItems() {
+          return getAlgoliaResults({
+            searchClient,
+            queries: [
+              {
+                indexName: 'instant_search',
+                query,
+                params: {
+                  hitsPerPage: 3,
+                },
+              },
+            ],
+          });
+        },
+
+      },
+    ];
+  },
 });
 
 search.addWidgets([
@@ -18,14 +46,11 @@ search.addWidgets([
 <article>
   <h1>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h1>
   <p>{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
-  <p>{{#helpers.highlight}}{ "attribute": "categories" }{{/helpers.highlight}}</p>
+  <p><img src="{{#helpers.highlight}}{ "attribute": "image" }{{/helpers.highlight}}"></p>
 </article>
 `,
     },
-  }),
-  instantsearch.widgets.pagination({
-    container: '#pagination',
-  }),
+  })
 ]);
 
 search.start();
