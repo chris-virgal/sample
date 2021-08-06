dae00dede75514cdb59f948765ebcff43d80d48d
chris <chris.virgal@gmail.com>
2021-08-06 22:59:33 +0200

    test sampl

---
+++ src/app.js
@@ -0,0 +1,31 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch('VTWN9LBTBB', '374b76e5ea3101f81afb7aa69ff9515e');
+
+const search = instantsearch({
+  indexName: 'test Chris',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+<article>
+  <h1>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h1>
+  <p>{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
+  <p>{{#helpers.highlight}}{ "attribute": "categories" }{{/helpers.highlight}}</p>
+</article>
+`,
+    },
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.start();
